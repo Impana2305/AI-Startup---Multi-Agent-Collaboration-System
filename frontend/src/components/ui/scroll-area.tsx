ab482ae7cede89d16d76a79d6a76118bc4c2146f
Impana L <impanal72@gmail.com>
2026-07-07 21:36:28 +0530

    Updated v2

---
+++ frontend/src/components/ui/scroll-area.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+interface ScrollAreaProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation?: "vertical" | "horizontal";
+}
+
+const ScrollArea = React.forwardRef<HTMLDivElement, ScrollAreaProps>(
+  ({ className, children, orientation = "vertical", ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "relative overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <div
+        className={cn(
+          "h-full w-full rounded-[inherit]",
+          orientation === "vertical" ? "overflow-y-auto" : "overflow-x-auto"
+        )}
+        style={{ scrollbarWidth: "thin" }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+);
+ScrollArea.displayName = "ScrollArea";
+
+export { ScrollArea };
